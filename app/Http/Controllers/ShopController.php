bc796f45efa4c32ed9d4f4a2d15c099ab832397f
rifqy abdallah <mrifqyabdallah@gmail.com>
2024-05-28 15:47:07 +0700

    solving all tests

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -15,6 +15,9 @@ class ShopController extends Controller
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
+        $resized_img = Image::make(storage_path('/app/shops/'.$filename));
+        $resized_img->resize(500, 500);
+        $resized_img->save(storage_path('/app/shops/resized-'.$filename));
 
         return 'Success';
     }
